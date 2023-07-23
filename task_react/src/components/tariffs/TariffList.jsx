9e9069a8b1a1f155c6465bc0d18f87a97ee68eb8
BarhatovaIrina <biriska@mail.ru>
2023-07-23 19:17:15 +0300

    react task 4 - state

--- task_react/src/components/tariffs/TariffList.jsx
+++ task_react/src/components/tariffs/TariffList.jsx
@@ -1,15 +1,27 @@
-
 import Tariff from './Tariff';
 import tariffs from './tariff_data.json';
 import styles from './TariffList.module.css';
+import { useState } from 'react';
 
 export default function TariffList() {
+    const [selected, setSelected] = useState(0);
+
+    const handleChangeSelected = (index) => {
+        setSelected(index);
+    }
     return (
         <div className={styles.list}>
             {
-                tariffs.map((item, index) =>
-                    <Tariff key={index} price={item.price} speed={item.speed} color={item.color} isSelected={item.isSelected} />
-                )
+                tariffs.map((item, index) => (
+                    <div key={index} onClick={() => handleChangeSelected(index)}>
+                        {(index === selected) ?
+                            <Tariff price={item.price} speed={item.speed} color={item.color} isSelected='true' />
+                            : <Tariff price={item.price} speed={item.speed} color={item.color} />
+                        }
+                    </div>
+
+
+                ))
             }
         </div>
     );
